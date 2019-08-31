ddf323db339e2c01d92adaa82cf30ed7bb8d3f3d
jpantana <josh.pantana@gmail.com>
2019-08-31 11:06:12 -0500

    fixed issues with classes and methods for better functionality

---
+++ LegoMinifiguresExercise/Classes/MiniFigure.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifiguresExercise.Classes
+{
+    class MiniFigure
+    {
+        public string Hat { get; set; }
+        public string Torso { get; set; }
+        public string Legs { get; set; }
+        public string Tools { get; set; }
+        public bool IsRested { get; set; }
+        public int TopSpeed { get; set; } = 12;
+        public int LegStrength { get; set; } = 100;
+
+        public MiniFigure()
+        {
+
+        }
+
+        public void Run()
+        {
+            Console.Write($"Your top running speed is {TopSpeed} mph");
+        }
+
+        public void Jump(int didJump)
+        {
+            LegStrength = LegStrength - didJump;
+
+            Console.WriteLine(LegStrength);
+
+            if (LegStrength < 10)
+            {
+                Console.WriteLine(LegStrength);
+            }
+
+        }
+
+        public void Sleep(int numHoursSlept)
+        {
+            
+            if (numHoursSlept < 8)
+            {
+                TopSpeed = 8; 
+                Console.WriteLine("You should sleep more.");
+            }
+            else if (numHoursSlept >= 8)
+            {
+                Console.WriteLine($"You are gloriously well-rested.");
+            }
+        }
+    }
+}
--- LegoMinifiguresExercise/Program.cs
+++ LegoMinifiguresExercise/Program.cs
@@ -10,52 +10,38 @@ namespace LegoMinifiguresExercise
             Console.WriteLine("Do you want to jump, run, or sleep?");
             var actionInput = Console.ReadLine();
 
+            var miniFigure = new MiniFigure();
+
             if (actionInput == "jump")
             {
-                int legoLegStrength = 100;
-                var jump = new Jump(legoLegStrength);
-
                 Console.WriteLine("Make Lego jump? y/n");
                 var makeJump = Console.ReadLine();
 
                 if (makeJump == "y")
                 {
-                    jump.isJumping(10);
+                    miniFigure.Jump(10);
                 }
                 if (makeJump == "n")
                 {
-
+                    Console.WriteLine("You're standing still.");
                 }
             }
             if (actionInput == "run")
             {
-                Console.WriteLine("Are you up for a run, or are you tired? y/n");
-                int topSpeed = 12;
+                Console.WriteLine("Are you too tired to run? y/n");
                 var isFatiguedInput = Console.ReadLine();
+                Console.Write("Oh well, I'm gonna make you run");
                 if (isFatiguedInput == "y")
                 {
-                    topSpeed = 2;
+                    miniFigure.TopSpeed = 2;
                 }
-                Console.Write($"Your top running speed is {topSpeed} mph");
-                var startRun = new Run(topSpeed);
-                
+                miniFigure.Run();
             }
             if (actionInput == "sleep")
             {
                 Console.WriteLine("How many hours did you sleep last night?");
                 int restInput = Int32.Parse(Console.ReadLine());
-                if (restInput < 8)
-                {
-                    var hasSleep = new Sleep(false);
-                    int topSpeed = 8;
-                    var startRun = new Run(topSpeed);
-                    Console.WriteLine($"You should sleep more.");
-                }
-                else if (restInput >= 8)
-                {
-                    var hasSleep = new Sleep(true);
-                    Console.WriteLine($"You are gloriously well-rested.");
-                }
+                miniFigure.Sleep(restInput);
             }
         }
     }
